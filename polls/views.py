d81fb989eb546c843d7a8dca67ff004cd01a094a
전윤호 <wjse0213@naver.com>
2022-06-11 23:26:32 +0900

    build:refactoring

--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,21 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.template import loader
 
 from .models import Question
 
 
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = {'latest_question_list': latest_question_list,}
+#     return HttpResponse(template.render(context, request))
+##### 간략하게 작성 ######
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'latest_question_list': latest_question_list}
+    return render  (request, 'polls/index.html', context)
+
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
